4f24b44ada66d8b1f5b29ede0283695aaa54d078
Bill Skrzypczak <bill@webbuildersconsulting.com>
2015-09-12 09:10:28 -0500

    Initial commit

---
+++ alo/src/main/java/com/example/MyClass.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class MyClass {
+
+    public static void main(String args[]) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a&&b));
+
+        System.out.println("a || b = " + (a||b) );
+
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+
+}
